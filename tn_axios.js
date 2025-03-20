501a64360f4eff56b5755ec419c94d65c896adad
Andreius_14 <Andreius_14@outlook.com>
2025-03-20 16:47:33 -0500

    Extra

--- tn_axios.js
+++ tn_axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+
 // ____            _
 //| __ )  __ _ ___(_) ___ ___
 //|  _ \ / _` / __| |/ __/ _ \
@@ -6,19 +7,89 @@ import axios from "axios";
 //|____/ \__,_|___/_|\___\___/
 //
 
+// -- Este Es Redundante
+// -- Aunque Facil de Leer
+// -- Preferible TryCath
+
+//async function jsonGet(ruta) {
+//  await axios
+//    .get(ruta)
+//    .then((response) => console.log(response.data))
+//    .catch((error) => console.error(error));
+//}
+
+//┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+//┃              FUNCTION                 ┃
+//┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+async function jsonGet(ruta) {
+  try {
+    const response = await axios.get(ruta);
+    console.log(response.data);
+  } catch (error) {
+    console.error("❌ Error en GET:", error);
+  }
+}
+
+async function jsonDelete(ruta) {
+  try {
+    const response = await axios.delete(ruta);
+    console.log(response.data);
+  } catch (error) {
+    console.error("❌ Error en DELETE:", error);
+  }
+}
+
+async function jsonPost(ruta, objetoJson) {
+  try {
+    const response = await axios.post(ruta, objetoJson);
+    console.log(response.data);
+  } catch (error) {
+    console.error("❌ Error en POST:", error);
+  }
+}
+
+async function jsonEdit(rutaEdit, editJson) {
+  try {
+    const response = await axios.patch(rutaEdit, editJson);
+    console.log(response.data.email);
+  } catch (error) {
+    console.error("❌ Error en PATCH:", error);
+  }
+}
+
+//{
+//      email: "donaymilin@ether.com",
+//}
+
+//┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+//┃                MAIN                   ┃
+//┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+async function main() {
+  const url = "http://localhost:3000";
+  const valor = `${url}/posts`;
+
+  const jsonNewValor = {
+    title: "Hola Mundo",
+    views: 500,
+  };
+
+  console.log("\n[Antes]");
+  await jsonGet(valor);
+  console.log("\n[NEW]");
+  await jsonPost(valor, jsonNewValor);
+  console.log("\n[Despues]");
+  await jsonGet(valor);
+}
+
+main(); // Ejecutamos la función principal
+
 /*
 ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-┃                 GET                   ┃
+┃        AXIOS - MULTIPLES GET          ┃
 ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 */
-//axios
-//  .get("https://jsonplaceholder.typicode.com/comments")
-//  .then((res) => {
-//    console.log(res.data);
-//  })
-//  .catch((err) => {
-//    console.error(err);
-//  });
 
 // GET - Multiple
 //axios
@@ -36,123 +107,18 @@ import axios from "axios";
 
 /*
 ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-┃                 POST                  ┃
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-*/
-//axios
-//  .post("https://jsonplaceholder.typicode.com/comments", {
-//    name: "Jackson Smith",
-//    email: "jsmith@example.com",
-//    body: "This is a piece of art.",
-//  })
-//  .then((res) => {
-//    console.log(res.data);
-//  })
-//  .catch((err) => {
-//    console.error(err);
-//  });
-
-/*
-┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-┃          UPDATE (PUT/PATCH)           ┃
+┃     AXIOS - PARAMETROS ESPECIFICOS    ┃
 ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 */
 
-// Before
-axios
-  .get("https://jsonplaceholder.typicode.com/comments/100")
-  .then((res) => {
-    console.log(res.data.email);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-// After
-axios
-  .patch("https://jsonplaceholder.typicode.com/comments/100", {
-    email: "donaymilin@ether.com",
-  })
-  .then((res) => {
-    console.log(res.data.email);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-/*
-┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-┃                DELETE                 ┃
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-*/
-axios
-  .delete("https://jsonplaceholder.typicode.com/comments/10")
-  .then((res) => {
-    console.log(res.data);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-//    _                                _
-//   / \__   ____ _ _ __  ______ _  __| | ___
-//  / _ \ \ / / _` | '_ \|_  / _` |/ _` |/ _ \
-// / ___ \ V / (_| | | | |/ / (_| | (_| | (_) |
-///_/   \_\_/ \__,_|_| |_/___\__,_|\__,_|\___/
-//
-
-/*
-┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-┃            CONFIGURACION              ┃
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-*/
-axios({
-  method: "get",
-  url: "https://jsonplaceholder.typicode.com/posts/1",
-  timeout: 5000, // 5 segundos
-  headers: { Authorization: "Bearer token123" },
-})
-  .then((response) => console.log(response.data))
-  .catch((error) => console.error("Error:", error));
-
-/*
-┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-┃                  ALL                  ┃
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-*/
-
-// llamadas Multiple
-axios
-  .all([
-    axios.get("https://jsonplaceholder.typicode.com/posts/1"),
-    axios.get("https://jsonplaceholder.typicode.com/posts/2"),
-  ])
-  .then(
-    axios.spread((post1, post2) => {
-      console.log("Post 1:", post1.data);
-      console.log("Post 2:", post2.data);
-    }),
-  )
-  .catch((error) => console.error("Error:", error));
-
-/*
-┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-┃            ASYNC Y AWAIT              ┃
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-*/
-
-async function obtenerDatos() {
-  try {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts/1",
-    );
-    console.log("Datos:", response.data);
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
-
-obtenerDatos();
+//axios({
+//  method: "get",
+//  url: "https://jsonplaceholder.typicode.com/posts/1",
+//  timeout: 5000, // 5 segundos
+//  headers: { Authorization: "Bearer token123" },
+//})
+//  .then((response) => console.log(response.data))
+//  .catch((error) => console.error("Error:", error));
 
 /*
 ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
@@ -160,13 +126,13 @@ obtenerDatos();
 ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 */
 
-const api = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com",
-  timeout: 5000,
-  headers: { "Content-Type": "application/json" },
-});
-
-api
-  .get("/posts/1")
-  .then((response) => console.log(response.data))
-  .catch((error) => console.error("Error:", error));
+//const api = axios.create({
+//  baseURL: "https://jsonplaceholder.typicode.com",
+//  timeout: 5000,
+//  headers: { "Content-Type": "application/json" },
+//});
+//
+//api
+//  .get("/posts/1")
+//  .then((response) => console.log(response.data))
+//  .catch((error) => console.error("Error:", error));
